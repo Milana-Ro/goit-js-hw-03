afb758b23b3b8d2c333bac4f4acf1c08e05abe1c
Milana-Ro <milana.roslyak@gmail.com>
2024-01-28 14:01:53 +0200

    js added

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,20 @@
 'use strict';
 
+function slugify(title) {
+
+    const stringToArray = title.split(" ");
+    let normalizedText;
+    const newArray = [];
+
+for (const element of stringToArray) {
+    normalizedText = element.toLowerCase();
+    newArray.push(normalizedText);
+}
+    const slug = newArray.join("-");
+    return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
